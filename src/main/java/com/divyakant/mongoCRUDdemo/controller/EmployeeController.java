65b1d00e2aed27a0728a6551f1b970f67254f7d5
Divyakant <divyakant846@gmail.com>
2022-07-29 11:52:42 +0530

    Using Model Template example

--- src/main/java/com/divyakant/mongoCRUDdemo/controller/EmployeeController.java
+++ src/main/java/com/divyakant/mongoCRUDdemo/controller/EmployeeController.java
@@ -3,9 +3,13 @@ package com.divyakant.mongoCRUDdemo.controller;
 import com.divyakant.mongoCRUDdemo.model.Employee;
 import com.divyakant.mongoCRUDdemo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -14,6 +18,9 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     @PostMapping
     public ResponseEntity<?> addEmployee(@RequestBody Employee employee)
@@ -40,10 +47,19 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeRepository.findById(id));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/id/{id}")
     public ResponseEntity<String> deleteById(@PathVariable Integer id)
     {
         employeeRepository.deleteById(id);
         return ResponseEntity.ok("Success!");
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<?> getEmployeeByName(@PathVariable String name)
+    {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is(name).and("gender").is("Female"));
+        List<Employee> employees = mongoTemplate.find(query, Employee.class);
+        return ResponseEntity.ok(employees);
     }
 }
